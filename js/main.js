feaf59f21c93afcb06457d98077ecd52464fefce
hanan <hananabdelmaseh9@gmail.com>
2021-06-03 13:23:31 +0200

    second change

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*var navItem = document.querySelectorAll(".nav-item");
+console.log(navItem);
+navItem.forEach(function() {
+    for (var i = 0; i <= navItem.length; i++) {
+        navItem[i].addEventListener('click', function() {
+            navItem[i].classList.toggle('active');
+
+        })
+    }
+});
+*/
